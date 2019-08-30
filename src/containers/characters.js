ad5cb06538f9c62b50e9d20c6e0bba9b5158ae4e
emily <mle4d@me.com>
2019-08-30 15:42:42 -0700

    character container

--- src/containers/characters.js
+++ src/containers/characters.js
@@ -1,22 +1,22 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import getcharacters from '../selectors/characterSelector';
+import getCharacters from '../selectors/characterSelector';
 import Characters from '../components/characters/Characters';
 import { fetchCharacters } from '../actions/characters';
 import { getCharacters } from '../services/avatarAPI';
 
 class characters extends PureComponent {
-  static propTypes = {
-    fetch: propTypes.func.isRequired,
-    characters: propTypes.array.isRequired,
+  static PropTypes = {
+    fetch: PropTypes.func.isRequired,
+    characters: PropTypes.array.isRequired,
   }
 
   componentDidMount() {
     this.props.fetch();
   }
   render() {
-    const { characters } = this.props;
+    const characters = this.props;
     return <Characters characters={characters} />;
   }
 }
@@ -33,4 +33,4 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(characters);
+)(getCharacters);
