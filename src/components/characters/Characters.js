bc30e7d4da248b76c8966c1b925ee0be7cc9de39
emily <mle4d@me.com>
2019-08-30 11:59:38 -0700

    file set up

---
+++ src/components/characters/Characters.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Character({ }) {
+  return (
+    <>
+    </>
+  );
+}
+
+Character.propTypes = {
+
+};
+
+export default Character;
