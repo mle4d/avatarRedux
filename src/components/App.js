b4fc2a84ff8bd0e2e538eb9d1bb17a5224a07c40
emily <mle4d@me.com>
2019-08-30 15:42:54 -0700

    added route in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import characters from '../containers/characters';
+
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/redux-avatar-promise" component={characters} />
+        <Route />
+      </Switch>
+    </Router>
+  );
 }
   
