b64970d7bedc1473da99ff7efcac78da6ab2562c
emily <mle4d@me.com>
2019-08-30 13:51:03 -0700

    character components

--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -4,12 +4,18 @@ import PropTypes from 'prop-types';
 function Character({ character }) {
   return (
     <>
+    <img src={character.img} />
+    <p>{character.name}</p>
     </>
   );
 }
 
 Character.propTypes = {
-
+  character: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired
+  }).isRequired
 };
 
 export default Character;
--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -2,13 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Characters({ characters }) {
+  const abCharacters = characters.map(character => (
+    <li key={character.id}>
+      <character character={character}></character>
+    </li>
+  ));
   return (
-    <>
-    </>
+    <ul>{abCharacters}</ul>
   );
 }
 
 Characters.propTypes = {
+  characters: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired
+  })).isRequired
 
 };
 
